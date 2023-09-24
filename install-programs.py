4a6b7c809555cda081e8214a8a608e66d41d578c
kerlorant <kertesz.lorant@gmail.com>
2023-09-24 23:34:13 +0200

    20230924

--- install-programs.py
+++ install-programs.py
@@ -9,7 +9,7 @@
 import subprocess
 
 # A megadott programok listája
-programs = ["program1", "program2", "program3"]
+programs = ["plank", "gvfs", "thunderbird", "ufw", "neofetch", "guake", "rednotebook", "gnome-disk-utility", "gparted", "htop", "lm_sensors", "wget", "discord", "audacity", "audacious", "keepassxc", "doublecmd-gtk2", "gimp", "vlc", "firefox", "gedit", "ark", "libreoffice-still", "libreoffice-still-hu", "brasero", "calibre", "transmission-gtk", "uget", "gnome-notes", "clipgrab", "avidemux-qt", "scribus", "librecad", "kicad", "simple-scan", "gthumb", "okular", "pacman-contrib", "onboard", "adobe-source-sans-pro-fonts", "cantarell-fonts", "noto-fonts", "ttf-bitstream-vera", "tf-dejavu", "ttf-droid", "ttf-hack", "ttf-inconsolata", "ttf-liberation", "ttf-roboto", "ttf-ubuntu-font-family", "tamsyn-font", "noto-fonts-emoji", "unace", "unrar", "zip", "unzip", "sharutils", "arj", "cabextract", "file-roller", "cups", "cups-pdf", "ghostscript", "gsfonts", "gutenprint", "gtk3-print-backends", "libcups", "hplip", "system-config-printer", "pulseaudio", "pulseaudio-alsa", "pavucontrol", "alsa-utils", "alsa-plugins", "alsa-lib", "alsa-firmware", "gstreamer", "gst-plugins-good", "gst-plugins-bad", "gst-plugins-base", "gst-plugins-ugly", "volumeicon", "playerctl", "android-file-transfer", "code", "pychess", "gnome-chess", "nemo", "git", "evince", "galculator", "calibre", "gedit", "micro"]
 
 # A nem telepített programok listája
 not_installed_programs = []
